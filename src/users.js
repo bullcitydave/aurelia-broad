8a3dee453004242719ca736442fe7767bbb477f4
Dave Seidman <bullcitydave@gmail.com>
2016-09-11 10:53:44 -0400

    Mod github page for own repos

--- src/users.js
+++ src/users.js
@@ -4,7 +4,7 @@ import 'fetch';
 
 @inject(HttpClient)
 export class Users {
-  heading = 'Github Users';
+  heading = 'My Github Repos';
   users = [];
 
   constructor(http) {
@@ -18,8 +18,36 @@ export class Users {
   }
 
   activate() {
-    return this.http.fetch('users')
+    return this.http.fetch('users/bullcitydave/repos?f33b4754f424b266f74ae2c7d9d820e97a179ca9')
       .then(response => response.json())
       .then(users => this.users = users);
   }
+
+//   var login = 'bullcitydave';
+//   var token = 'f33b4754f424b266f74ae2c7d9d820e97a179ca9';
+//
+// // Get github API URLs
+// var repoURL = 'https://api.github.com/users/' + login + '/repos?' + token;
+// var userURL = 'https://api.github.com/users/' + login + '?' + token;
+// var eventURL = 'https://api.github.com/users/' + login + '/events?' + token;
+// var starredURL = 'https://api.github.com/users/' + login + '/starred?' + token;
+//
+// $.getJSON(userURL).done(function(userData){
+//     var userView = $('.user-template').html();
+//     userData.starredCount = _.size($.getJSON(starredURL));
+//     $('#user-info').append(_.template(userView,userData));
+// });
+
+// WHY IS responseJSON not accessible here?
+// var j =   $.getJSON(userURL).done(function(userData){
+//     return userData.responseJSON;
+// });
+
+
+// $.getJSON(repoURL).done(function(repoData){
+//     var repoView = $('.repo-template').html();
+//     for (var i = 0; i < (_.size(repoData)) && i < 5 ; i++) {
+//         $('#popular-repos').append(_.template(repoView,repoData[i]));
+//     }
+// });
 }
