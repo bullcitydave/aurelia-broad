eb009c6222508008f8d5e272464e759717a1bb56
Dave Seidman <bullcitydave@gmail.com>
2016-09-11 09:45:12 -0400

    Add tag and strikethrough

--- src/app.js
+++ src/app.js
@@ -5,13 +5,21 @@ export class App {
     this.heading = "Todos";
     this.todos = [];
     this.todoDescription = '';
+    this.todoTags = '';
   }
 
   addTodo() {
     if (this.todoDescription) {
-      this.todos.push(new Todo(this.todoDescription));
+      this.todos.push(new Todo(this.todoDescription, this.todoTags));
       this.todoDescription = '';
     }
+  }
+
+  addTag(todo, tag) {
+      let index = this.todos.indexOf(todo);
+      if (index !== -1) {
+        this.todos[index].tags.push(tag);
+      }
   }
 
   removeTodo(todo) {
--- src/todo.js
+++ src/todo.js
@@ -1,6 +1,29 @@
 export class Todo {
-  constructor(description) {
+  constructor(description, tags) {
     this.description = description;
     this.done = false;
+    // this.getTags = getTags();
+    this.tags = tags;
   }
+
+  // getTags(){
+  //     return this.parse(tags);
+  // }
+
+
+//
+// parse(csString) {
+//     parse(csString, ',', true);
+// }
+//
+// parse(csString, delimiter, doTrim) {
+//     csEls = csstring.split(delimiter);
+//     csEls.forEach(function (value) {
+//         if (doTrim) {
+//             value = value.trim();
+//         }
+//         console.log(value + '**');
+//     });
+// }
+
 }
